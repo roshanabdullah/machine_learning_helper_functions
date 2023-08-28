d40ee49fdb92aec4b2fa1396774907dad4d83e63
Abdullah Roshan <85522748+roshanabdullah@users.noreply.github.com>
2023-08-28 20:27:00 +0500

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,29 @@
+# Plot the validation and training curves separately
+# Function to plot the curves while in evaluating phase of the model
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation methods
+
+  """
+
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+
+  epochs = range(len(history.history["loss"])) # How many epochs did we run for?
+
+  # Plot loss 
+  plt.plot(epochs, loss, label="training_loss")
+  plt.plot(epochs, val_loss, label="val_loss")
+  plt.title("loss")
+  plt.xlabel("epochs")
+  plt.legend()
+
+
+  # Plot accuracy
+  plt.plot(epochs, accuracy, label="training_accuracy")
+  plt.plot(epochs, val_accuracy, label="val_accuracy")
+  plt.title("accuracy")
+  plt.xlabel("epochs")
